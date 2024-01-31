ad447b05a4337bc0f78cd8ade3a4da2a323fa8d0
manish20002 <manishthirani202@gmail.com>
2024-01-31 11:42:24 +0530

    this is me

---
+++ Manish/src/main.js
@@ -0,0 +1,20 @@
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/mobile-nav.css';
+import '../styles/components/hero.css';
+import '../styles/components/about.css';
+import '../styles/components/exp.css';
+import '../styles/components/contact.css';
+import '../styles/components/project.css';
+import '../styles/components/footer.css';
+import '../styles/utils.css';
+
+
+import mobileNav from './utils/mobile-nav';
+import darkMode from './utils/dark-mode';
+
+
+
+darkMode();
+mobileNav();
